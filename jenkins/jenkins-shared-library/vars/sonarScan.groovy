e36b5bf80a9fd28f7b5ab2aabe1870d16e61ebda
vinay.reddy <kvinay80130@gmail.com>
2026-08-01 22:42:03 +0530

    changes

--- jenkins/jenkins-shared-library/vars/sonarScan.groovy
+++ jenkins/jenkins-shared-library/vars/sonarScan.groovy
@@ -6,7 +6,7 @@ def call(Map params = [:]) {
 
     withSonarQubeEnv(sonarServer) {
         sh """
-        mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.11.0:sonar \
+        mvn org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \
         -Dsonar.projectKey=${projectKey} \
         -Dsonar.projectName=${projectName}
         """
