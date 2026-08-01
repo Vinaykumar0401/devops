ed5c0b63ae13178e9ce2d43a72eb57b7e884df30
vinay.reddy <kvinay80130@gmail.com>
2026-08-01 22:25:40 +0530

    sonar error

--- jenkins/jenkins-shared-library/vars/sonarScan.groovy
+++ jenkins/jenkins-shared-library/vars/sonarScan.groovy
@@ -6,7 +6,7 @@ def call(Map params = [:]) {
 
     withSonarQubeEnv(sonarServer) {
         sh """
-        mvn org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \
+        mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.11.0:sonar \
         -Dsonar.projectKey=${projectKey} \
         -Dsonar.projectName=${projectName}
         """
